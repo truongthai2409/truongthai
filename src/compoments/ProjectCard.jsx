9e93ed553a81f8a98fba799e0271f49b933e488d
Truong Thai <52000714@student.tdtu.edu.vn>
2024-03-24 00:04:21 +0700

    feat: add Project images

--- src/compoments/ProjectCard.jsx
+++ src/compoments/ProjectCard.jsx
@@ -4,14 +4,20 @@
 import { Fragment } from "react";
 
 // eslint-disable-next-line no-unused-vars
-const ProjectCard = ({projectName}) => {
+const ProjectCard = ({ projectName, ProjectURL, ProjectImage }) => {
     return (
         <Fragment>
             <div className="w-[250px] flex flex-col h-[full] p-5 m-5 border rounded-lg bg-white">
-                <img className="rounded-xl image-shadow" src="https://media.themoviedb.org/t/p/w300_and_h450_bestv2//sMp34cNKjIb18UBOCoAv4DpCxwY.jpg" alt="" />
+                <img className="rounded-xl image-shadow h-[330px] object-contain" src={ProjectImage}alt="" />
                 <div className="flex flex-col ">
                     <h1 className="flex justify-center px-3 py-3">{projectName}</h1>
-                    <button className="px-6 py-3 mt-auto capitalize transition duration-150 ease-out border hover:ease-in -full rounded-3xl hover:bg-slate-700 hover:text-white">Visited</button>
+
+                    <div className="px-6 py-3 mt-auto text-center capitalize transition duration-150 ease-out border hover:ease-in -full rounded-3xl hover:bg-slate-700 hover:text-white">
+                        <a className="block" href={ProjectURL} target="_blank">
+                            Visited
+                        </a>
+                    </div>
+
                 </div>
             </div>
         </Fragment>
--- src/layout/Projects.jsx
+++ src/layout/Projects.jsx
@@ -6,14 +6,10 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
-
+import { ProjectName, ProjectURL, ProjectImage } from '../utils/DataProject';
 
 const Projects = () => {
-    const ProjectName = {
-        pops: 'POPS System',
-        figma: 'Sweet Shop',
-        chatapp: 'Chat App'
-    }
+  
 
     gsap.registerPlugin(ScrollTrigger);
     const container = useRef();
@@ -38,9 +34,9 @@ const Projects = () => {
                     <div className="max-w-screen-xl project">
                         <h1 className="p-4 text-4xl text-center text-white">My Projects</h1>
                         <div className="relative grid grid-cols-3 grid-rows-1 gap-5 bg-white rounded-xl">
-                            <ProjectCard projectName={ProjectName.pops}></ProjectCard>
-                            <ProjectCard projectName={ProjectName.figma}></ProjectCard>
-                            <ProjectCard projectName={ProjectName.chatapp}></ProjectCard>
+                            <ProjectCard projectName={ProjectName.pops} ProjectURL={ProjectURL.pops} ProjectImage={ProjectImage.pops}></ProjectCard>
+                            <ProjectCard projectName={ProjectName.figma} ProjectURL={ProjectURL.figma} ProjectImage={ProjectImage.figma}></ProjectCard>
+                            <ProjectCard projectName={ProjectName.movie} ProjectURL={ProjectURL.movie} ProjectImage={ProjectImage.movie}></ProjectCard>
                         </div>
                     </div>
                 </div>
