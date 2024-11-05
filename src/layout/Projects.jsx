bc727178c0786824f4bfa0552cd5e9773e477264
Truong Thai <52000714@student.tdtu.edu.vn>
2024-11-05 16:48:59 +0700

    Feat: Fix layot Project

--- src/layout/Projects.jsx
+++ src/layout/Projects.jsx
@@ -1,39 +1,34 @@
-// import React from 'react';
-
 import { Element } from "react-scroll";
-import ProjectCard from "../compoments/ProjectCard";
 import gsap from 'gsap';
+import ProjectCard from "../compoments/ProjectCard";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import { ProjectName, ProjectURL, ProjectImage } from '../utils/DataProject';
 
 const Projects = () => {
-  
-
     gsap.registerPlugin(ScrollTrigger);
     const container = useRef();
-   
+
     useGSAP(() => {
-        gsap.from(".project",{
+        gsap.from(".project", {
             opacity: 0,
             y: -50,
             duration: 3,
             scrollTrigger: {
                 trigger: ".project",
                 end: "50px",
-                // markers: true
             }
-        })
-    }, {scope: container})
+        });
+    }, { scope: container });
 
     return (
         <Element name="Projects">
-            <section ref={container} className="h-[100vh] bg-image">
+            <section ref={container} className="h-auto sm:h-[100vh] bg-image py-10 sm:py-0">
                 <div className="flex items-center justify-center">
                     <div className="max-w-screen-xl project">
-                        <h1 className="p-4 text-4xl text-center text-white">My Projects</h1>
-                        <div className="relative grid grid-cols-3 grid-rows-1 gap-5 bg-white rounded-xl">
+                        <h1 className="p-4 text-3xl text-center text-white sm:text-4xl">My Projects</h1>
+                        <div className="relative grid grid-cols-1 gap-5 p-4 bg-white sm:grid-cols-3 rounded-xl sm:p-0">
                             <ProjectCard projectName={ProjectName.pops} ProjectURL={ProjectURL.pops} ProjectImage={ProjectImage.pops}></ProjectCard>
                             <ProjectCard projectName={ProjectName.figma} ProjectURL={ProjectURL.figma} ProjectImage={ProjectImage.figma}></ProjectCard>
                             <ProjectCard projectName={ProjectName.movie} ProjectURL={ProjectURL.movie} ProjectImage={ProjectImage.movie}></ProjectCard>
